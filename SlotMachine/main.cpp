8bb26792ad42d890ada24a1d90da3c2c7a6e2970
Stephen Szwiec <55602870+StephenSzwiec@users.noreply.github.com>
2020-02-11 14:37:02 -0600

    main method

---
+++ SlotMachine/main.cpp
@@ -0,0 +1,20 @@
+/*
+* written by Stephen Szwiec, 11 February 2020 for CS161 at Minot State University
+*/
+
+/*revision v1.0, 5 February 2020
+*	- implemented Marsenne Twister for randomness in CSpinner
+*	- implemented runtime and logic
+*/
+
+/*revision v1.1, 11 February 2020
+*	- bill functionality added, including end of run auditing
+* 	- windows screen blanking now done in a less-vulnerable manner
+*	- split runtime.cpp and CSlot.h from each other 
+*/
+	
+int main() {
+	CSlot machine;
+	machine.run();
+	return 0; 
+}
