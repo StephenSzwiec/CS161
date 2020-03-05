9cd12ebccdb51e956a9af634c774ff04dac38af0
Stephen Szwiec <55602870+StephenSzwiec@users.noreply.github.com>
2020-03-05 11:42:09 -0600

    Adding windows port

---
+++ CrazyEights/Windows/Runtime.cpp
@@ -0,0 +1,15 @@
+
+/*
+* written by Stephen Szwiec, 26 February 2020 for CS161 at Minot State University
+*/
+
+
+#include "CDeck.h"
+#include "CPlayer.h"
+#include "CCrazyEights.h"
+
+int main() {
+	CCrazyEights runner;
+	runner.run();
+	return 0;
+}
